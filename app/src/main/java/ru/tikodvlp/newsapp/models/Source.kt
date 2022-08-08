c79a329f7136dce27bed252f771573512fee93b7
Tigran <tigranma@gmail.com>
2022-08-08 17:47:52 +0300

    Lesson-3

---
+++ app/src/main/java/ru/tikodvlp/newsapp/models/Source.kt
@@ -0,0 +1,6 @@
+package ru.tikodvlp.newsapp.models
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/ru/tikodvlp/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package ru.tikodvlp.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "edaad67244c0475a834c29396b2d35ce"
+    }
+}
