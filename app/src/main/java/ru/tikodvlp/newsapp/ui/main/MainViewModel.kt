02906ed92827a8300f20017b27d1f7db0a5b318e
Tigran <tigranma@gmail.com>
2022-08-10 22:16:40 +0300

    Lesson-5 (p.1)

--- app/src/main/java/ru/tikodvlp/newsapp/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/tikodvlp/newsapp/ui/main/MainViewModel.kt
@@ -1,34 +1,10 @@
 package ru.tikodvlp.newsapp.ui.main
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
-import ru.tikodvlp.newsapp.data.api.TestRepo
-import ru.tikodvlp.newsapp.models.NewsResponse
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val repository: TestRepo): ViewModel() {
+class MainViewModel @Inject constructor(): ViewModel() {
 
-    private val _all = MutableLiveData<NewsResponse>()
-    val all: LiveData<NewsResponse>
-    get() = _all
-
-    init {
-        getAll()
-    }
-
-    fun getAll() = viewModelScope.launch {
-        repository.getAll().let {
-            if (it.isSuccessful) {
-                _all.postValue(it.body())
-            } else {
-                Log.d("checkData", "Failed to load articles: ${it.errorBody()}")
-            }
-        }
-    }
 }
