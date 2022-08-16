0cccb417e87a2fda8b24bb0f8bd45f2f80f2fbda
Tigran <tigranma@gmail.com>
2022-08-16 15:35:05 +0300

    Lesson-8

--- app/src/main/java/ru/tikodvlp/newsapp/di/AppModule.kt
+++ app/src/main/java/ru/tikodvlp/newsapp/di/AppModule.kt
@@ -1,14 +1,19 @@
 package ru.tikodvlp.newsapp.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.tikodvlp.newsapp.data.api.NewsService
+import ru.tikodvlp.newsapp.data.db.ArticleDao
+import ru.tikodvlp.newsapp.data.db.ArticleDatabase
 import ru.tikodvlp.newsapp.utils.Constants.Companion.BASE_URL
 import javax.inject.Singleton
 
@@ -38,4 +43,19 @@ object AppModule {
             .client(okHttpClient())
             .build()
             .create(NewsService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideArticleDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(
+            context,
+            ArticleDatabase::class.java,
+            "article_database"
+        ).build()
+
+    @Provides
+    fun provideArticleDao(appDatabase: ArticleDatabase): ArticleDao {
+        return appDatabase.getArticleDao()
+    }
+
 }
